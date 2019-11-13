2a468b05263841a2e0c4f35e19f5b326a981da41
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-11-13 20:57:01 +0530

    Heroku push

--- src/index.js
+++ src/index.js
@@ -4,18 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// plugins styles from node_modules
-import "react-notification-alert/dist/animate.css";
-import "react-perfect-scrollbar/dist/css/styles.css";
-// plugins styles downloaded
-import "assets/vendor/fullcalendar/dist/fullcalendar.min.css";
-import "assets/vendor/sweetalert2/dist/sweetalert2.min.css";
-import "assets/vendor/select2/dist/css/select2.min.css";
-import "assets/vendor/quill/dist/quill.core.css";
-import "assets/vendor/nucleo/css/nucleo.css";
-import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
+
 // core styles
-import "assets/scss/VSM-dashboard-pro-react.scss?v1.0.0";
+import "./assets/scss/VSM-dashboard-pro-react.scss";
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
